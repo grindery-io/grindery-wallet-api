43dfbc6bf37686bc8afaeaf73b0c3cf481d5f13a
Egor Miliukov <milukove@gmail.com>
2023-10-31 23:08:54 +0200

    add app stats endpoint

--- src/routes/v2/stats.ts
+++ src/routes/v2/stats.ts
@@ -4,6 +4,7 @@ import telegramHashIsValid from '../../utils/telegramHashIsValid';
 import {
   REWARDS_COLLECTION,
   TRANSFERS_COLLECTION,
+  USERS_COLLECTION,
 } from '../../utils/constants';
 
 const router = express.Router();
@@ -52,6 +53,88 @@ router.get('/', telegramHashIsValid, async (req, res) => {
       rewards,
       referrals,
     });
+  } catch (error) {
+    console.error(
+      `Error getting user ${res.locals.userId} stats`,
+      JSON.stringify(error)
+    );
+    return res.status(500).send({ msg: 'An error occurred', error });
+  }
+});
+
+/**
+ * GET /v2/stats/app
+ *
+ * @summary Get App stats
+ * @description Gets app stats
+ * @tags Stats
+ * @security BearerAuth
+ * @return {object} 200 - Success response with stats object
+ * @example response - 200 - Success response example
+ * {
+ *    "users": {
+ *      "total": 100,
+ *      "new": {
+ *        "hour": 1,
+ *        "day": 10,
+ *      },
+ *      "withContacts": {
+ *        "total": 20,
+ *        "new": {
+ *          "hour": 0,
+ *          "day": 1
+ *        }
+ *      }
+ *    }
+ * }
+ */
+router.get('/app', async (req, res) => {
+  console.log(`App stats requested`);
+  const timestamp = new Date().getTime();
+  try {
+    const db = await Database.getInstance(req);
+
+    const stats = {
+      users: {
+        total: await db
+          .collection(USERS_COLLECTION)
+          .countDocuments({ webAppOpened: { $exists: true } }),
+        new: {
+          hour: await db.collection(USERS_COLLECTION).countDocuments({
+            webAppOpenedFirstDate: {
+              $gte: new Date(new Date().getTime() - 60 * 60 * 1000),
+            },
+          }),
+          day: await db.collection(USERS_COLLECTION).countDocuments({
+            webAppOpenedFirstDate: {
+              $gte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+            },
+          }),
+        },
+        withContacts: {
+          total: await db
+            .collection(USERS_COLLECTION)
+            .countDocuments({ telegramSession: { $exists: true } }),
+          new: {
+            hour: await db.collection(USERS_COLLECTION).countDocuments({
+              telegramSessionSavedDate: {
+                $gte: new Date(new Date().getTime() - 60 * 60 * 1000),
+              },
+            }),
+            day: await db.collection(USERS_COLLECTION).countDocuments({
+              telegramSessionSavedDate: {
+                $gte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+              },
+            }),
+          },
+        },
+      },
+    };
+
+    console.log(
+      `App stats request completed in ${new Date().getTime() - timestamp}ms`
+    );
+    return res.status(200).send(stats);
   } catch (error) {
     console.error(
       `Error getting user ${res.locals.userId} stats`,
