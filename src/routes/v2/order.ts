2edca465f04a61294eb22fddfb0c1505c45e97a9
Egor Miliukov <milukove@gmail.com>
2023-12-29 01:25:48 +0200

    add order-related routes

--- src/routes/v2/order.ts
+++ src/routes/v2/order.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request } from 'express';
 import axios from 'axios';
 import telegramHashIsValid from '../../utils/telegramHashIsValid';
 const router = express.Router();
@@ -36,6 +36,127 @@ router.get('/quote', telegramHashIsValid, async (req, res) => {
   } catch (error) {
     console.error(
       `Error getting g1 convertion quote for user ${res.locals.userId}`,
+      JSON.stringify(error)
+    );
+    return res.status(500).send({ success: false, error: 'An error occurred' });
+  }
+});
+
+/**
+ * POST /v2/order
+ *
+ * @summary Place an order
+ * @description Places an order and sends g1 tokens
+ * @tags Order
+ * @security BearerAuth
+ * @param {string} quoteId.body - gx exchange quote ID
+ * @return {object} 200 - Success response
+ */
+router.post('/', telegramHashIsValid, async (req, res) => {
+  console.log(`User [${res.locals.userId}] requested order`);
+  if (!req.body.quoteId || typeof req.body.quoteId !== 'string') {
+    return res.status(400).json({ error: 'Invalid quote ID' });
+  }
+  try {
+    const result = await axios.post(
+      `https://bot-auth-api.grindery.org/v1/tge/pre-order`,
+      {
+        quoteId: req.body.quoteId,
+        userTelegramID: res.locals.userId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      }
+    );
+    console.log(`User [${res.locals.userId}] order request completed`);
+    return res.status(200).send(result.data);
+  } catch (error) {
+    console.error(
+      `Error placing g1 order for user ${res.locals.userId}`,
+      JSON.stringify(error)
+    );
+    return res.status(500).send({ success: false, error: 'An error occurred' });
+  }
+});
+
+/**
+ * GET /v2/order/{orderId}
+ *
+ * @summary Get order status
+ * @description Gets order status
+ * @tags Order
+ * @security BearerAuth
+ * @return {object} 200 - Success response
+ */
+router.get('/status', telegramHashIsValid, async (req: Request, res) => {
+  console.log(`User [${res.locals.userId}] requested order status`);
+  if (!req.params.orderId) {
+    return res.status(400).json({ error: 'Invalid order ID' });
+  }
+  try {
+    const result = await axios.get(
+      `https://bot-auth-api.grindery.org/v1/tge/order-status?orderId=${req.params.orderId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      }
+    );
+    console.log(`User [${res.locals.userId}] order status request completed`);
+    return res.status(200).send(result.data);
+  } catch (error) {
+    console.error(
+      `Error getting g1 order status for user ${res.locals.userId}`,
+      JSON.stringify(error)
+    );
+    return res.status(500).send({ success: false, error: 'An error occurred' });
+  }
+});
+
+/**
+ * PATCH /v2/order/{orderId}
+ *
+ * @summary Pay order usd amount
+ * @description Updates order and sends usd transaction in selected token
+ * @tags Order
+ * @security BearerAuth
+ * @param {string} orderId.path - order ID
+ * @param {string} tokenAdddress.body - selected token address
+ * @param {string} chainId.body - selected chain id
+ * @return {object} 200 - Success response
+ */
+router.patch('/:orderId', telegramHashIsValid, async (req, res) => {
+  console.log(`User [${res.locals.userId}] requested order payment`);
+  if (!req.params.orderId) {
+    return res.status(400).json({ error: 'Invalid order ID' });
+  }
+  if (!req.body.tokenAddress || typeof req.body.tokenAddress !== 'string') {
+    return res.status(400).json({ error: 'Invalid token address' });
+  }
+  if (!req.body.chainId || typeof req.body.chainId !== 'string') {
+    return res.status(400).json({ error: 'Invalid chain ID' });
+  }
+  try {
+    const result = await axios.patch(
+      `https://bot-auth-api.grindery.org/v1/tge/complete-order`,
+      {
+        orderId: req.params.orderId,
+        tokenAddress: req.body.tokenAddress,
+        chainId: req.body.chainId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      }
+    );
+    console.log(`User [${res.locals.userId}] order payment request completed`);
+    return res.status(200).send(result.data);
+  } catch (error) {
+    console.error(
+      `Error paying g1 order for user ${res.locals.userId}`,
       JSON.stringify(error)
     );
     return res.status(500).send({ success: false, error: 'An error occurred' });
