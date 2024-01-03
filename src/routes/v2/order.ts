e83b16a782e9f861646dac48738dea9fdfdadb2d
Egor Miliukov <milukove@gmail.com>
2024-01-03 04:38:24 +0200

    update order routes

--- src/routes/v2/order.ts
+++ src/routes/v2/order.ts
@@ -1,8 +1,6 @@
 import express, { Request } from 'express';
 import axios from 'axios';
 import telegramHashIsValid from '../../utils/telegramHashIsValid';
-import { Database } from '../../db/conn';
-import { GX_ORDER_COLLECTION, USERS_COLLECTION } from '../../utils/constants';
 const router = express.Router();
 
 /**
@@ -26,7 +24,7 @@ router.get('/quote', telegramHashIsValid, async (req, res) => {
   }
   try {
     const result = await axios.get(
-      `https://bot-auth-api.grindery.org/v1/tge/conversion-information?g1Quantity=${req.query.convert}&usdQuantity=${req.query.add}`,
+      `https://bot-auth-api.grindery.org/v1/tge/quote?g1Quantity=${req.query.convert}&usdQuantity=${req.query.add}&userTelegramID=${res.locals.userId}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
@@ -61,7 +59,7 @@ router.post('/', telegramHashIsValid, async (req, res) => {
   }
   try {
     const result = await axios.post(
-      `https://bot-auth-api.grindery.org/v1/tge/pre-order`,
+      `https://bot-auth-api.grindery.org/v1/tge/order`,
       {
         quoteId: req.body.quoteId,
         userTelegramID: res.locals.userId,
@@ -77,6 +75,39 @@ router.post('/', telegramHashIsValid, async (req, res) => {
   } catch (error) {
     console.error(
       `Error placing g1 order for user ${res.locals.userId}`,
+      error
+    );
+    return res.status(500).send({ success: false, error: 'An error occurred' });
+  }
+});
+
+/**
+ * GET /v2/order/list
+ *
+ * @summary Get all user orders
+ * @description Gets all user orders
+ * @tags Order
+ * @security BearerAuth
+ * @return {object} 200 - Success response
+ */
+router.get('/list', telegramHashIsValid, async (req: Request, res) => {
+  console.log(`User [${res.locals.userId}] requested orders list`);
+
+  try {
+    const result = await axios.get(
+      `https://bot-auth-api.grindery.org/v1/tge/orders?userTelegramID=${res.locals.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      }
+    );
+
+    console.log(`User [${res.locals.userId}] orders list request completed`);
+    return res.status(200).send(result.data);
+  } catch (error) {
+    console.error(
+      `Error getting current order status for user ${res.locals.userId}`,
       JSON.stringify(error)
     );
     return res.status(500).send({ success: false, error: 'An error occurred' });
@@ -92,14 +123,14 @@ router.post('/', telegramHashIsValid, async (req, res) => {
  * @security BearerAuth
  * @return {object} 200 - Success response
  */
-router.get('/status', telegramHashIsValid, async (req: Request, res) => {
+router.get('/:orderId', telegramHashIsValid, async (req: Request, res) => {
   console.log(`User [${res.locals.userId}] requested order status`);
   if (!req.params.orderId) {
     return res.status(400).json({ error: 'Invalid order ID' });
   }
   try {
     const result = await axios.get(
-      `https://bot-auth-api.grindery.org/v1/tge/order-status?orderId=${req.params.orderId}`,
+      `https://bot-auth-api.grindery.org/v1/tge/order?orderId=${req.params.orderId}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
@@ -118,63 +149,20 @@ router.get('/status', telegramHashIsValid, async (req: Request, res) => {
 });
 
 /**
- * GET /v2/order
- *
- * @summary Get user's order status
- * @description Gets user's current order status
- * @tags Order
- * @security BearerAuth
- * @return {object} 200 - Success response
- */
-router.get('/', telegramHashIsValid, async (req: Request, res) => {
-  console.log(`User [${res.locals.userId}] requested current order status`);
-
-  try {
-    const db = await Database.getInstance(req);
-    const user = await db
-      .collection(USERS_COLLECTION)
-      .findOne({ userTelegramID: res.locals.userId });
-
-    if (!user || (user.isBanned && user.isBanned !== 'false')) {
-      return res.status(400).json({ error: 'User is banned' });
-    }
-
-    const order = await db.collection(GX_ORDER_COLLECTION).findOne({
-      userTelegramID: res.locals.userId,
-    });
-
-    if (!order?.orderId) {
-      return res.status(404).json({ error: 'Order not found' });
-    }
-
-    console.log(
-      `User [${res.locals.userId}] current order status request completed`
-    );
-    return res.status(200).send(order);
-  } catch (error) {
-    console.error(
-      `Error getting current order status for user ${res.locals.userId}`,
-      JSON.stringify(error)
-    );
-    return res.status(500).send({ success: false, error: 'An error occurred' });
-  }
-});
-
-/**
- * PATCH /v2/order/{orderId}
+ * PATCH /v2/order
  *
  * @summary Pay order usd amount
  * @description Updates order and sends usd transaction in selected token
  * @tags Order
  * @security BearerAuth
- * @param {string} orderId.path - order ID
+ * @param {string} orderId.body - order ID
  * @param {string} tokenAdddress.body - selected token address
  * @param {string} chainId.body - selected chain id
  * @return {object} 200 - Success response
  */
-router.patch('/:orderId', telegramHashIsValid, async (req, res) => {
+router.patch('/', telegramHashIsValid, async (req, res) => {
   console.log(`User [${res.locals.userId}] requested order payment`);
-  if (!req.params.orderId) {
+  if (!req.body.orderId) {
     return res.status(400).json({ error: 'Invalid order ID' });
   }
   if (!req.body.tokenAddress || typeof req.body.tokenAddress !== 'string') {
@@ -185,11 +173,12 @@ router.patch('/:orderId', telegramHashIsValid, async (req, res) => {
   }
   try {
     const result = await axios.patch(
-      `https://bot-auth-api.grindery.org/v1/tge/complete-order`,
+      `https://bot-auth-api.grindery.org/v1/tge/order`,
       {
-        orderId: req.params.orderId,
+        quoteId: req.body.orderId,
         tokenAddress: req.body.tokenAddress,
         chainId: req.body.chainId,
+        userTelegramID: res.locals.userId,
       },
       {
         headers: {
